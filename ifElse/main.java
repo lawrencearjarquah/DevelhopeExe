6c8424ae58366e55574ed4674a27a5a9e154e931
lawrence <odoyebre@gmail.com>
2023-06-14 18:14:36 +0200

    esercizio sul if__else

--- ifElse/main.java
+++ ifElse/main.java
@@ -1,4 +1,16 @@
 package Esercizi.ifElse;
 
 public class main {
+    public static void main (String[] args) {
+        /* Define a variable called myName and assign your name to it
+Using the if-else statement, print if the number of letters in your name is even or odd*/
+
+        String myName = "Lawrence";
+        if (myName.length() % 2 == 0) {
+            System.out.println("è paro");
+        }
+        else {
+            System.out.println("è disparo");
+        }
+    }
 }
